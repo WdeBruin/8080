8ffe0eaf91d07bb4eeead3fcce36dbc4297273ea
Wouter de Bruin <wouterdebruin88@gmail.com>
2023-08-13 00:00:25 +0200

    implemented empty opcode function. ambition to tdd

---
+++ State.cs
@@ -0,0 +1,27 @@
+namespace cpu;
+
+public struct ConditionCodes
+{
+    public byte z;
+    public byte s;
+    public byte p;
+    public byte cy;
+    public byte ac;
+    public byte pad;
+}
+
+public struct State8080 
+{
+    public byte a;
+    public byte b;
+    public byte c;
+    public byte d;
+    public byte e;
+    public byte h;
+    public byte l;
+    public UInt16 sp;
+    public UInt16 pc;
+    public unsafe byte* memory;
+    public ConditionCodes cc;
+    public byte int_enable; 
+}
